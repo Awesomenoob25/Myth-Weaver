d50b274f9b318fa8fb663f4e7c7a568436ed4cc3
mojom <mojom@NEW_XPS>
2025-05-13 17:07:44 +0800

    Add project files.

---
+++ Myth Weaver/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Myth_Weaver
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
